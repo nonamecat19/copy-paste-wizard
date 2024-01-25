91b6daa5cd51daad0fbfb4bc3c7743509ed34f7c
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-01-25 17:20:21 +0200

    feat: buttons from navbar to settings page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,14 @@
-import { Button } from '@/components/ui/button'
-import { JsonService } from '@/services/json.service'
-import { useDataStore } from '@/store/useDataStore'
 import logo from '@/assets/logo.jpg'
 import NavLink from '@/components/NavLink.tsx'
 
 export default function Navbar() {
-  const dataStore = useDataStore()
-
-  async function importHandle() {
-    const data = await JsonService.importData()
-    if (!data) {
-      return
-    }
-    dataStore.setData(data)
-  }
-
-  function resetHandle() {
-    JsonService.resetLocalData()
-    dataStore.clearData()
-  }
-
-  async function exportHandle() {
-    await JsonService.exportData(dataStore.data)
-  }
-
   return (
     <nav className="border-b px-4 border-zinc-800 h-14 flex justify-between items-center">
       <img alt="Logo" src={logo} className="h-12" />
 
       <div className="flex gap-2">
-        <Button onClick={resetHandle} variant="outline">
-          Reset
-        </Button>
-
-        <Button onClick={importHandle} variant="outline">
-          Import
-        </Button>
-
-        <Button onClick={exportHandle} variant="outline">
-          Export
-        </Button>
-        <NavLink to="/settings">Settings</NavLink>
         <NavLink to="/">Home</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
       </div>
     </nav>
   )
--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,3 +1,40 @@
+import { Button } from '@/components/ui/button.tsx'
+import { useDataStore } from '@/store/useDataStore.ts'
+import { JsonService } from '@/services/json.service.ts'
+
 export default function Settings() {
-  return <div>Settings</div>
+  const dataStore = useDataStore()
+
+  async function importHandle() {
+    const data = await JsonService.importData()
+    if (!data) {
+      return
+    }
+    dataStore.setData(data)
+  }
+
+  function resetHandle() {
+    JsonService.resetLocalData()
+    dataStore.clearData()
+  }
+
+  async function exportHandle() {
+    await JsonService.exportData(dataStore.data)
+  }
+
+  return (
+    <div className="m-2 flex flex-col gap-2 ">
+      <Button onClick={resetHandle} variant="outline">
+        Reset data
+      </Button>
+
+      <Button onClick={importHandle} variant="outline">
+        Import from file
+      </Button>
+
+      <Button onClick={exportHandle} variant="outline">
+        Export to file
+      </Button>
+    </div>
+  )
 }
